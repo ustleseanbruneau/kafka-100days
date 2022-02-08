9ebb0e94a8612a95bff0f544f7adc22606a7c5e1
UST - L.B <eagandog2009@gmail.com>
2022-02-07 21:44:24 -0600

    added Kafka admin scripts

---
+++ kafka-admin/k-server-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kafka-server-start.sh /home/sar/kafka_2.12-2.8.0/config/server.properties
---
+++ kafka-admin/z-server-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zookeeper-server-start.sh /home/sar/kafka_2.12-2.8.0/config/zookeeper.properties
