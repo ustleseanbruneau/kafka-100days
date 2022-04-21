9f3693a26ca0954bdbf51e55f38e784341516a32
LB <eagandog2009@gmail.com>
2022-04-21 10:48:39 -0500

    additional notes

--- kafka-admin/k-server-start.sh
+++ kafka-admin/k-server-start.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-${HOME}/kafka_2.13-3.1.0/bin/kafka-server-start.sh ${HOME}/kafka_2.13-3.1.0/config/server.properties
+${HOME}/kafka_2.13-3.1.0/bin/kafka-server-start.sh -daemon ${HOME}/kafka_2.13-3.1.0/config/server.properties
+
+
+#if PATH is set - run in foreground
+#${HOME}/kafka_2.13-3.1.0/bin/kafka-server-start.sh ${HOME}/kafka_2.13-3.1.0/config/server.properties
+
+#if PATH is set and run process in background
+#${HOME}/kafka_2.13-3.1.0/bin/kafka-server-start.sh -daemon ${HOME}/kafka_2.13-3.1.0/config/server.properties
+
+# check log file
+tail -n 15 ~/kafka_2.13-3.1.0/logs/server.log
+
+# check if process is running
+echo "ruok" | nc localhost 9092; echo
+OR
+echo "ruok" | nc localhost 9282; echo
+
+
+
--- kafka-admin/z-server-start.sh
+++ kafka-admin/z-server-start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-${HOME}/kafka_2.13-3.1.0/bin/zookeeper-server-start.sh ${HOME}/kafka_2.13-3.1.0/config/zookeeper.properties
+${HOME}/kafka_2.13-3.1.0/bin/zookeeper-server-start.sh -daemon ${HOME}/kafka_2.13-3.1.0/config/zookeeper.properties
+
+#if PATH is set
+#zookeeper-server-start.sh ${HOME}/kafka_2.13-3.1.0/config/zookeeper.properties
+
+#if PATH is set and run process in background
+#zookeeper-server-start.sh -daemon ${HOME}/kafka_2.13-3.1.0/config/zookeeper.properties
+
+# check log file
+tail -n 15 ~/kafka_2.13-3.1.0/logs/zookeeper-gc.log
+
+# check if process is running
+echo "ruok" | nc localhost 2181; echo
+
+
